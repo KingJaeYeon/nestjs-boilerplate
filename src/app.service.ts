88f25f043e9585e57c0735c9834bbb57028b7831
pjy <wodus331@gmail.com>
2024-12-29 20:24:44 +0900

    prettier eslint setting

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const a = 5234234 + 123892183982319389231 + 4873198729817398712897398723198 + 3128737289;
+    return `Hello World!`;
   }
 }
