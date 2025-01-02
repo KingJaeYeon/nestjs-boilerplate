8840d599bbfa622ac2cf358ec7681888a8143326
pjy <wodus331@vanillacode.net>
2025-01-02 16:04:07 +0900

    prisma dao 최종

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,13 +2,8 @@ import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { UserDao, userExtension } from './extensions/user.extension';
 import { AccountDao, accountExtension } from './extensions/account.extension';
-import { PrismaExtensions } from '../decorators/prisma-extensions.decorator';
-
-// 모든 DAO 확장 결합
-const allExtensions = [userExtension, accountExtension];
 
 @Injectable()
-@PrismaExtensions(allExtensions)
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   userDao: UserDao;
   accountDao: AccountDao;
@@ -18,7 +13,6 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
     this.userDao = userExtension(this).user;
     this.accountDao = accountExtension(this).account;
   }
-
   async onModuleInit() {
     await this.$connect();
   }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,8 +7,8 @@ export class UserService {
   constructor(private readonly db: PrismaService) {}
 
   async createUser() {
-    const user = await this.db.userDao.findByIdOrThrow('cm5ahxxle0000ad09p74d7ix8');
-    console.log(user);
+    const userDao = await this.db.userDao.findByIdOrThrow('cm5ahxxle0000ad09p74d7ix8');
+    console.log('userDao:', userDao);
     const account = await this.db.accountDao.findByEmailOrThrow('cm5ahxxle0000ad09p74d7ix8', Provider.LOCAL);
     console.log(account);
     return 'createUser';
