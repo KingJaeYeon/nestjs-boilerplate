cb138de256949d26b1fa8b7172ff0dd5fedb6e5e
pjy <wodus331@gmail.com>
2025-01-06 01:43:30 +0900

    exception test

--- src/common/exception/error.code.ts
+++ src/common/exception/error.code.ts
@@ -12,4 +12,14 @@ export const ErrorCode: { [key: string]: IErrorCode } = {
     code: 'USER-001',
     message: 'User not found',
   },
+  USER_ALREADY_EXISTS: {
+    status: HttpStatus.CONFLICT,
+    code: 'USER-002',
+    message: 'User already exists',
+  },
+  USER_INVALID_PASSWORD: {
+    status: HttpStatus.BAD_REQUEST,
+    code: 'USER-003',
+    message: 'Invalid password',
+  },
 };
